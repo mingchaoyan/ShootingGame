574e03d73bd5d74260afb7d4e2aacae13a4419e3
mingchaoyan <mingchaoyan@gmail.com>
2014-09-14 23:09:45 +0800

    添加起始界面

---
+++ Assets/Scripts/TitleScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 48;
+
+        GUI.skin.label.alignment = TextAnchor.LowerCenter;
+
+        GUI.Label(new Rect(0, 30, Screen.width, 100), "太空大战");
+
+        if (GUI.Button(new Rect(Screen.width * 0.5f - 100, Screen.height * 0.7f, 200, 30), "开始游戏"))
+        {
+            Application.LoadLevel("level1");
+        }
+    }
+}
