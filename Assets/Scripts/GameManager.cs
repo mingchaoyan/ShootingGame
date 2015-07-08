149a8d986dd70a9532b6f7aca7acdc2a16609fe1
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 18:03:19 +0800

    使用matrix4x4方案适配好UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,128 +2,96 @@
 using System.Collections;
 
 [AddComponentMenu("MyGame/GameManager")]
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+	public int m_score = 0;
+	public static int m_hiscore = 0;
+	protected Player m_player;
+	public AudioClip m_musicClip;
+	protected AudioSource m_Audio;
 
-    public static GameManager Instance;
+	void Awake ()
+	{
+		Instance = this;
+	}
 
-    //得分
-    public int m_score = 0;
-
-    //纪录
-    public static int m_hiscore = 0;
-
-    //主角
-    protected Player m_player;
-
-    // 背景音乐
-    public AudioClip m_musicClip;
-
-    // 声音源
-    protected AudioSource m_Audio;
-
-    void Awake()
-    {
-        Instance = this;
-    }
-
-	// Use this for initialization
-	void Start () {
-
-        m_Audio = this.audio;
-
-        // 获取主角
-        GameObject obj = GameObject.FindGameObjectWithTag("Player");
-        if (obj != null)
-        {
-            m_player = obj.GetComponent<Player>();
-        }
-	
+	void Start ()
+	{
+		m_Audio = this.audio;
+		GameObject obj = GameObject.FindGameObjectWithTag ("Player");
+		if (obj != null) {
+			m_player = obj.GetComponent<Player> ();
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-        // 循环播放背景音乐
-        if (!m_Audio.isPlaying)
-        {
-            m_Audio.clip = m_musicClip;
-            m_Audio.Play();
+	void Update ()
+	{
+		if (!m_Audio.isPlaying) {
+			m_Audio.clip = m_musicClip;
+			m_Audio.Play ();
            
-        }
+		}
 
-        // 暂停游戏
-        if (Time.timeScale > 0 && Input.GetKeyDown(KeyCode.Escape))
-        {
-            Time.timeScale = 0;
-        }
+		if (Time.timeScale > 0 && Input.GetKeyDown (KeyCode.Escape)) {
+			Time.timeScale = 0;
+		}
 	}
 
-    void OnGUI()
-    {
-        // 游戏暂停
-        if (Time.timeScale == 0)
-        {
-            // 继续游戏按钮
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 50, Screen.height * 0.4f, 100, 30), "继续游戏"))
-            {
-                Time.timeScale = 1;
-            }
+	void OnGUI ()
+	{
+		float ratioW = System.Convert.ToSingle (Screen.width) / 540f;
+		float ratioH = System.Convert.ToSingle (Screen.height) / 960f;
+		GUI.matrix = Matrix4x4.TRS (new Vector3 (0, 0, 0), 
+		                           Quaternion.identity, new Vector3 (ratioW, ratioH, 1));
 
-            // 退出游戏按钮
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 50, Screen.height * 0.6f, 100, 30), "退出游戏"))
-            {
-                // 退出游戏
-                Application.Quit();
-            }
-        }
+		if (Time.timeScale == 0) {
+			GUI.skin.button.fontSize = 40;
+			if (GUI.Button (new Rect (540 * 0.5f - 540f / 3 / 2, 960 * 0.4f, 540f / 3f, 960f / 15f), 
+			                "继续游戏")) {
+				Time.timeScale = 1;
+			}
+
+			GUI.skin.button.fontSize = 40;
+			if (GUI.Button (new Rect (540 * 0.5f - 540f / 3 / 2, 960 * 0.6f, 540f / 3f, 960f / 15f), 
+			                "退出游戏")) {
+				Application.Quit ();
+			}
+		}
         
-        int life = 0;
-        if (m_player != null)
-        {
-            // 获得主角的生命值
-            life = (int)m_player.m_life;
-        }
-        else // game over
-        {
-            
-            // 放大字体
-            GUI.skin.label.fontSize = 50;
+		int life = 0;
+		if (m_player != null) {
+			life = (int)m_player.m_life;
+		} else { 
+			GUI.skin.label.fontSize = 50;
+			GUI.skin.label.alignment = TextAnchor.LowerCenter;
+			GUI.Label (new Rect (0, 960 * 0.5f, 540, 100), "游戏失败");
 
-            // 显示游戏失败
-            GUI.skin.label.alignment = TextAnchor.LowerCenter;
-            GUI.Label(new Rect(0, Screen.height * 0.2f, Screen.width, 60), "游戏失败");
+			GUI.skin.button.fontSize = 40;
+			if (GUI.Button (new Rect (540f / 3f, 960f * 8 / 10f, 540f / 3f, 960f / 15f), 
+			                "再试一次")) {
+				Application.LoadLevel (Application.loadedLevelName);
+			}
+		}
 
-            GUI.skin.label.fontSize = 20;
+		GUI.skin.label.fontSize = 40;
+		GUI.Label (new Rect (5, 5, 100, 100), "装甲 " + life);
 
-            // 显示按钮
-            if (GUI.Button(new Rect(Screen.width * 0.5f - 50, Screen.height * 0.5f, 100, 30), "再试一次"))
-            {
-                // 读取当前关卡
-                Application.LoadLevel(Application.loadedLevelName);
-            }
-        }
+		GUI.skin.label.fontSize = 40;
+		GUI.skin.label.alignment = TextAnchor.LowerCenter;
+		GUI.Label (new Rect (540f / 2f - 50, 5, 100, 100), "纪录 " + m_hiscore);
 
-        GUI.skin.label.fontSize = 15;
+		GUI.skin.label.fontSize = 40;
+		GUI.Label (new Rect (540f / 2f - 50, 5, 100, 100), "得分 " + m_score);
 
-        // 显示主角生命
-        GUI.Label(new Rect(5, 5, 100, 30), "装甲 " + life);
+		GUI.matrix = Matrix4x4.identity;
+	}
 
-        // 显示最高分
-        GUI.skin.label.alignment = TextAnchor.LowerCenter;
-        GUI.Label(new Rect(0, 5, Screen.width, 30), "纪录 " + m_hiscore);
+	public void AddScore (int point)
+	{
+		m_score += point;
 
-        // 显示当前得分
-        GUI.Label(new Rect(0, 25, Screen.width, 30), "得分 " + m_score);
-      
-    }
-
-    // 增加分数
-    public void AddScore( int point )
-    {
-        m_score += point;
-
-        // 更新高分纪录
-        if (m_hiscore < m_score)
-            m_hiscore = m_score;
-    }
+		if (m_hiscore < m_score)
+			m_hiscore = m_score;
+	}
 }
--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -6,15 +6,22 @@ public class TitleScene : MonoBehaviour
 
 	void OnGUI ()
 	{
-		GUI.skin.label.fontSize = 58;
-		GUI.skin.label.alignment = TextAnchor.LowerCenter;
-		GUI.skin.button.fontSize = 45;
-		GUI.Label (new Rect (Screen.width / 3f, Screen.height / 20f, 
-		                     Screen.width / 3f, Screen.height / 10f), "太空大战");
+		float ratioW = System.Convert.ToSingle (Screen.width) / 540f;
+		float ratioH = System.Convert.ToSingle (Screen.height) / 960f;
+		GUI.matrix = Matrix4x4.TRS (new Vector3 (0, 0, 0), 
+		                           Quaternion.identity, new Vector3 (ratioW, ratioH, 1));
 
-		if (GUI.Button (new Rect (Screen.width / 3f, Screen.height * 8 / 10f, 
-		                          Screen.width / 3f, Screen.height / 15f), "开始游戏")) {
+		GUI.skin.label.fontSize = 40;
+		GUI.skin.label.alignment = TextAnchor.LowerCenter;
+		GUI.Label (new Rect (540f / 3f, 960f / 20f, 
+		                     540f / 3f, 960f / 10f), "太空大战");
+
+		GUI.skin.button.fontSize = 40;
+		if (GUI.Button (new Rect (540f / 3f, 960f * 8 / 10f, 
+		                          540f / 3f, 960f / 15f), "开始游戏")) {
 			Application.LoadLevel ("level1");
 		}
+
+		GUI.matrix = Matrix4x4.identity;
 	}
 }
