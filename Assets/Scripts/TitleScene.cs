00fd1dd8bf707fbb372e8e521e92b64c530dee24
mingchaoyan <mingchaoyan@gmail.com>
2015-07-08 16:31:48 +0800

    调整一个场景的UI适配

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -1,29 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TitleScene : MonoBehaviour {
+public class TitleScene : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnGUI ()
+	{
+		GUI.skin.label.fontSize = 58;
+		GUI.skin.label.alignment = TextAnchor.LowerCenter;
+		GUI.skin.button.fontSize = 45;
+		GUI.Label (new Rect (Screen.width / 3f, Screen.height / 20f, 
+		                     Screen.width / 3f, Screen.height / 10f), "太空大战");
+
+		if (GUI.Button (new Rect (Screen.width / 3f, Screen.height * 8 / 10f, 
+		                          Screen.width / 3f, Screen.height / 15f), "开始游戏")) {
+			Application.LoadLevel ("level1");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    void OnGUI()
-    {
-        GUI.skin.label.fontSize = 48;
-
-        GUI.skin.label.alignment = TextAnchor.LowerCenter;
-
-        GUI.Label(new Rect(0, 30, Screen.width, 100), "太空大战");
-
-        if (GUI.Button(new Rect(Screen.width * 0.5f - 100, Screen.height * 0.7f, 200, 30), "开始游戏"))
-        {
-            Application.LoadLevel("level1");
-        }
-    }
 }
